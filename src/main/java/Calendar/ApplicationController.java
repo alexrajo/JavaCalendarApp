cb1d3605a5329248f9a5e2a79950892e6bdf1eb8
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2022-03-06 13:51:04 +0100

    Endret på kontroller og fxml-filen

--- src/main/java/Calendar/ApplicationController.java
+++ src/main/java/Calendar/ApplicationController.java
@@ -1,8 +1,31 @@
 package Calendar;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class ApplicationController {
+    private Calendar calendar;
 
+    @FXML
+    private GridPane CalGrid;
+
+    @FXML
+    public void initialize() {
+        for (int i = 7; i < 37; i++) {
+            CalGrid.add(createCalElem(i-6), i % 7, i / 7);
+        }
+    }
+
+    @FXML
+    public Text createCalElem(int i) {
+        Text text = new Text(String.valueOf(i));
+        text.setX(50);
+        text.setY(50);
+        return text;
+    }
 }
